cfb3d875a61df3e45d72414070f769b4836da6bd
CdCordoba <cd.cordoba13@gmail.com>
2022-06-21 12:13:55 -0300

    comiit

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,55 @@
 // Inputs and Textarea events
 const data = {
-    nameInput: '',
-    emailInput: '',
-    messageInput: '';
-}
+  nameInput: "",
+  emailInput: "",
+  messageInput: "",
+};
 
-const nameInput = document.querySelector('#nameInput');
-const emailInput = document.querySelector('#emailInput');
-const messageInput = document.querySelector('#messageInput');
-const form = document.querySelector('.form');
+const nameInput = document.querySelector("#nameInput");
+const emailInput = document.querySelector("#emailInput");
+const messageInput = document.querySelector("#messageInput");
+const form = document.querySelector(".form");
 
-nameInput.addEventListener('input', validateForm);
-emailInput.addEventListener('input', validateForm);
-messageInput.addEventListener('input', validateForm);
+nameInput.addEventListener("input", validateForm);
+emailInput.addEventListener("input", validateForm);
+messageInput.addEventListener("input", validateForm);
 
 // Submit Event
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Validate form 
-    const { nameInput, emailInput, messageInput } = data;
-    console.log(nameInput);
-    console.log(emailInput);
-    console.log(messageInput);
-    if (nameInput === '' || emailInput === '' || messageInput === '') {
-        showAlert('All fields are required', true);
-        return; //Stop running the function 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Validate form
+  const { nameInput, emailInput, messageInput } = data;
+  console.log(nameInput);
+  console.log(emailInput);
+  console.log(messageInput);
+  if (nameInput === "" || emailInput === "" || messageInput === "") {
+    showAlert("All fields are required", true);
+    return; //Stop running the function
+  }
 
-    }
-
-    showAlert('Your message has been sent successfully')
-
+  showAlert("Your message has been sent successfully");
 });
 
 //Save data from the form
 function validateForm(e) {
-    data[e.target.id] = e.target.value;
+  data[e.target.id] = e.target.value;
 }
 
 //Show an alert message
 function showAlert(message, error = null) {
-    const alert = document.createElement('P');
-    alert.textContent = message;
-    if (error) { // Show an error message
-        alert.classList.add('error');
-    } else { // Show a sent message
-        alert.classList.add('sent')
-    }
-    form.appendChild(alert);
+  const alert = document.createElement("P");
+  alert.textContent = message;
+  if (error) {
+    // Show an error message
+    alert.classList.add("error");
+  } else {
+    // Show a sent message
+    alert.classList.add("sent");
+  }
+  form.appendChild(alert);
 
-    //Desappear after 2 seconds
-    setTimeout(() => {
-        alert.remove();
-    }, 2000);
+  //Desappear after 2 seconds
+  setTimeout(() => {
+    alert.remove();
+  }, 2000);
 }
